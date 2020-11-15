5bab7818f83c7d3d4c580722de5f21c0af786534
Xiaoyu861971996 <ydd861971996@163.com>
2020-11-15 15:16:00 +0800

    lesson1.py

---
+++ lesson1.py
@@ -0,0 +1,77 @@
+'''import keyword
+
+print(keyword.kwlist)'''
+
+# 第一个脚本
+# print("hello lemon")
+
+# 换行
+# title=("hello\nlemon")
+# print(title,type(title))
+
+# title = '''hello lemon
+# lemon1
+# lemon2'''
+# print(title)
+
+# 步长
+# title='hello lemon'
+# print(title[0:4])
+# print(title[0::1])   #步长为1，从第一个字符到最后一个字符
+# print(title[0::2])   #步长为2，从第一个字符到最后一个字符
+# print(title[0::-1])
+# print(title[1::-2])
+
+# 格式化
+# print("%s同学,工作年限：%d年"%('李四',2))
+
+# title='hello'
+# name='zhangsan'
+# print("%s %s"%(title,name))
+
+# print("{1}同学,工作年限：{0}年".format(2,'李四'))
+
+# name="张三"
+# nianxian=2
+# print(f"{name}同学,工作年限：{nianxian}年")
+
+# len()函数
+# title="hello lemon1234"
+# print(len(title))
+# print(title[2:len(title)])
+
+# 替换
+# title="hello lemon"
+# title1=title.replace('lemon','zhangsan')
+# print(title1)
+
+# 查找
+# title="hello lemon"
+# print(title.find('h'))
+# print(title.index('e'))
+
+# sum=1
+# # sum1=sum+1
+# # print(sum1)
+
+# sum=1
+# sum+=1
+# print(sum)
+
+# 比较运算符
+# print(3<2)
+
+# 逻辑运算符
+# print(1==1 and 0==0)
+# print(0==1 or 1==1)
+# not非
+# print(not 1>2)
+
+# title="hello lemon"
+# # print('m'in title)
+# print('f'not in title and 0)
+
+
+
+
+
